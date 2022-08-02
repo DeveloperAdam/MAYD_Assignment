ceb9677e18daead2a8a92ea3b986730b580e7877
Adam Noor <adamnoor@Salmans-Macbook-Pro.local>
2022-08-03 01:09:20 +0400

    master: svg's added, UI is in progress

---
+++ app/src/main/java/com/adam/mayd_assignment/ui/ShortlyActivity.kt
@@ -0,0 +1,12 @@
+package com.adam.mayd_assignment.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adam.mayd_assignment.R
+
+class ShortlyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shortly)
+    }
+}
